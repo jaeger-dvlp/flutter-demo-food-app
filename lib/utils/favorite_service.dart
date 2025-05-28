1bad9e187a1509790af7b79c32a5b65179418a39
jaeger-dvlp <omer.kayalar0101@icloud.com>
2025-05-28 20:30:55 +0300

    feat: favorite service

---
+++ lib/utils/favorite_service.dart
@@ -0,0 +1,35 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FavoriteService {
+  static const String _key = "favorite_ids";
+
+  static Future<void> addFavorite(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> currentFavs = prefs.getStringList(_key) ?? [];
+
+    if (!currentFavs.contains(id)) {
+      currentFavs.add(id);
+      await prefs.setStringList(_key, currentFavs);
+    }
+  }
+
+  static Future<void> deleteFavorite(String id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final List<String> currentFavs = prefs.getStringList(_key) ?? [];
+
+    if (currentFavs.contains(id)) {
+      currentFavs.remove(id);
+      await prefs.setStringList(_key, currentFavs);
+    }
+  }
+
+  static Future<List<String>> getFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getStringList(_key) ?? [];
+  }
+
+  static Future<bool> isFavorite(String id) async {
+    final list = await getFavorites();
+    return list.contains(id);
+  }
+}
