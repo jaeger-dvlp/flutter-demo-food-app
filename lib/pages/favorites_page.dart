8464bc6cb29da23feb0a62ab14527c2418b01b0a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2025-05-28 20:12:30 +0300

    feat: create fav meals paage

---
+++ lib/pages/favorites_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:food_app_demo/utils/api_service.dart';
+import '../models/recipe_model.dart';
+import '../pages/detail_page.dart';
+import '../utils/favorite_service.dart';
+
+class FavoritesPage extends StatefulWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  State<FavoritesPage> createState() => _FavoritesPageState();
+}
+
+class _FavoritesPageState extends State<FavoritesPage> {
+  List<Recipe> favoriteRecipes = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    final favoriteIds = await FavoriteService.getFavorites();
+    List<Recipe> loadedFavorites = [];
+
+    for (final id in favoriteIds) {
+      final recipe = await ApiService().getRecipeById(id);
+      if (recipe != null) {
+        loadedFavorites.add(recipe);
+      }
+    }
+
+    setState(() {
+      favoriteRecipes = loadedFavorites;
+      isLoading = false;
+    });
+  }
+
+  Future<void> _removeFavorite(String id) async {
+    await FavoriteService.deleteFavorite(id);
+    await _loadFavorites();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isLoading) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+    return Scaffold(
+      appBar: AppBar(title: const Text('Favorite Meals')),
+      body: favoriteRecipes.isEmpty
+          ? const Center(child: Text('Please add favorite meals.'))
+          : ListView.builder(
+              itemCount: favoriteRecipes.length,
+              itemBuilder: (context, index) {
+                final recipe = favoriteRecipes[index];
+
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(recipe.strMealThumb),
+                  ),
+                  title: Text(recipe.strMeal),
+                  subtitle: Text(recipe.strCategory),
+                  trailing: IconButton(
+                    onPressed: () {
+                      _removeFavorite(recipe.idMeal);
+                    },
+                    icon: const Icon(Icons.delete),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => DetailPage(recipe: recipe),
+                      ),
+                    ).then((_) => {_loadFavorites()});
+                  },
+                );
+              },
+            ),
+    );
+  }
+}
